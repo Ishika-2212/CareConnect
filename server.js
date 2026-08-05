557685d4bad320e3a95201600b44f3ea4cf54681
Ishika-2212 <goyal.ishika65@gmail.com>
2026-08-05 23:49:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,11 +24,9 @@ mysqlCon.connect(function (err) {
         console.log(err.message);
 })
 //--------------------------------------------//
-var nodemailer = require("nodemailer");
+const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
+    service: "gmail",
     auth: {
         user: process.env.EMAIL,
         pass: process.env.EMAIL_PASS
